edefd3df93d121a12fe5bdd7fa9be8042a6c57cd
Sedat Guclu <sguclu@finaxys.com>
2016-01-14 00:36:40 +0100

    typo

--- 02-PACKAGE-ATM/atm-startup.sh
+++ 02-PACKAGE-ATM/atm-startup.sh
@@ -4,8 +4,8 @@
 cd /tmp/ATM-SERVER/lib
 runningJar=$(ls bluebank-atm-*.jar)
 
-echo -- launching ATM CLIENT (NGINX) : nginx
+echo " -- launching ATM CLIENT (NGINX) : nginx"
 nginx
 
-echo -- launching ATM SERVER : java -jar ${runningJar} org.bluebank.Application
+echo " -- launching ATM SERVER : java -jar ${runningJar} org.bluebank.Application"
 java -jar ${runningJar} org.bluebank.Application
