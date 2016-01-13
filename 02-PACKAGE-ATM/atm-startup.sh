31ac02e5d67e6cda716a52d6defbbfea69a30121
Sedat Guclu <sguclu@finaxys.com>
2016-01-13 21:33:35 +0100

    server jar loaded

--- 02-PACKAGE-ATM/atm-startup.sh
+++ 02-PACKAGE-ATM/atm-startup.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
 
 # ATM run server
-cd /tmp/ATM-SERVER
-for lib in WEB-INF/lib/*.jar; do CLASSPATH=${CLASSPATH}:${lib} ; done
+cd /tmp/ATM-SERVER/lib
+runningJar=$(ls bluebank-atm-*.jar)
 echo -- launching ATM SERVER : java -cp $CLASSPATH org.bluebank.Application
-nohup java -cp $CLASSPATH org.bluebank.Application &
-
-# ATM client run
-cd node-client
-echo -- launching ATM CLIENT : npm start
-npm start
+java -jar ${runningJar} org.bluebank.Application
